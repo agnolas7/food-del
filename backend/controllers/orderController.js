a40bae83b48344976ced25c90e19d2eab49f3083
agnolas7 <124791247+agnolas7@users.noreply.github.com>
2024-10-22 17:52:02 +0800

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,8 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js"
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const frontend_URL = "https://food-del-frontend-t4bv.onrender.com/"
 // Placing User Order for Frontend
 const placeOrder = async (req, res) => {
 
