fd22b093d8ce8fb79007e6f0b920c44460efa577
agnolas7 <124791247+agnolas7@users.noreply.github.com>
2024-10-24 13:55:56 +0800

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 export const assets ={
-    logo,
+    AdminPanelLogo,
     add_icon,
     order_icon,
     profile_image,
