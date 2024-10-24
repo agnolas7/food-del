5e69be7adf042b8d022b95ded4732455e3d2a1a2
agnolas7 <124791247+agnolas7@users.noreply.github.com>
2024-10-24 11:58:04 +0800

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className='header-contents'>
-                <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-                <Link to="/menu"> {/* Use Link to navigate to /menu */}
-                    <button>View Menu</button>
-                </Link>
-            </div>
-        </div>
-    )
-}
+  const handleScrollToMenu = () => {
+    const menuSection = document.getElementById("explore-menu");
+    if (menuSection) {
+      menuSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients and culinary expertise. Our
+          mission is to satisfy your cravings and elevate your dining
+          experience, one delicious meal at a time.
+        </p>
+        <button onClick={handleScrollToMenu}>View Menu</button> {/* Smooth scroll to menu */}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
